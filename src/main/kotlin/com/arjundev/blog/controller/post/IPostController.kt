8d04807d4085ad27b995237c62c06358e5cef21e
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-05-03 04:41:15 +0530

    fix imports

--- src/main/kotlin/com/arjundev/blog/controller/post/IPostController.kt
+++ src/main/kotlin/com/arjundev/blog/controller/post/IPostController.kt
@@ -4,8 +4,6 @@ import com.arjundev.blog.payloads.*
 import jakarta.validation.Valid
 import org.springdoc.core.annotations.ParameterObject
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
--- src/main/kotlin/com/arjundev/blog/repository/IPostRepo.kt
+++ src/main/kotlin/com/arjundev/blog/repository/IPostRepo.kt
@@ -3,7 +3,6 @@ package com.arjundev.blog.repository
 import com.arjundev.blog.entities.Category
 import com.arjundev.blog.entities.Post
 import com.arjundev.blog.entities.User
-import org.springdoc.core.annotations.ParameterObject
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
